efa03fd8f238406d8de997318606053638da1c1c
helzze <sebastiansancaowo@gmail.com>
2024-09-23 21:23:48 -0500

    pregunta 2 sebastian sanca

---
+++ src/main/java/pe/dsw/evaluacion/edu/evaluacion_t1_dsw/serviceGenerarArchivos/FileService.java
@@ -0,0 +1,55 @@
+package pe.dsw.evaluacion.edu.evaluacion_t1_dsw.serviceGenerarArchivos;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.io.*;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Service
+public class FileService {
+
+    private static final String FILE_PATH_1 = "infoXD.txt";
+    private static final String FILE_PATH_2 = "infoXD2.txt";
+    private static final String FILE_PATH_3 = "infoXD3.txt";
+
+    //archivo 1 - 10 segundos de demora
+    public void crearArchivo1() throws IOException {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(FILE_PATH_1);
+             DataOutputStream dataOutputStream = new DataOutputStream(fileOutputStream)) {
+            dataOutputStream.writeUTF("Archivo 1, creado con 10 segundos de delay");
+            dataOutputStream.writeInt(2024);
+
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //archivo 2 - 5 segundos de demora
+    public void crearArchivo2() throws IOException {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(FILE_PATH_2);
+             DataOutputStream dataOutputStream = new DataOutputStream(fileOutputStream)) {
+            dataOutputStream.writeUTF("Archivo 2, creado con 5 segundos de delay");
+            dataOutputStream.writeInt(2024);
+
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //archivo 3 - 7 segundos de demora
+    public void crearArchivo3() throws IOException {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(FILE_PATH_3);
+             DataOutputStream dataOutputStream = new DataOutputStream(fileOutputStream)) {
+            dataOutputStream.writeUTF("Archivo 3, creado con 7 segundos de delay");
+            dataOutputStream.writeInt(2024);
+
+            TimeUnit.SECONDS.sleep(7);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/pe/dsw/evaluacion/edu/evaluacion_t1_dsw/EvaluacionT1DswApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.dsw.evaluacion.edu.evaluacion_t1_dsw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluacionT1DswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
